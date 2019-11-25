e35f22e55190d2161475fa374ecc3dccd3876f55
Jeffry Hesse <jhesse@sonatype.com>
2019-11-25 12:38:48 -0900

    CRAN WIP

---
+++ ext-src/packages/r/RUtils.ts
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { exec } from "../../utils/exec";
+import { PackageDependenciesHelper } from "../PackageDependenciesHelper";
+import { RPackage } from './RPackage';
+
+export class RUtils {
+  public async getDependencyArray(): Promise<Array<RPackage>> {
+    try {
+      let {stdout, stderr } = await exec(
+        `echo "str(ip <- installed.packages(fields = "Version"))
+        ip[, c(1,3:5)]" | R --slave`
+        , {
+        cwd: PackageDependenciesHelper.getWorkspaceRoot(),
+        env: {
+          PATH: process.env.PATH
+        }
+      });
+
+      if (stdout != "" && stderr === "") {
+        return Promise.resolve(this.parseRInstall(stdout));
+      } else {
+        return Promise.reject(
+          new Error(
+            "Error occurred in generating dependency list. Check that you have R installed/"
+          )
+        );
+      }
+    } catch (e) {
+      return Promise.reject(
+        `R script failed, try running it manually to see what went wrong: ${e.error}`
+      );
+    }
+  }
+
+  private parseRInstall(dependencyTree: string): Array<RPackage> {
+    const dependencies: string = dependencyTree;
+    console.debug(dependencies);
+    console.debug(
+      "------------------------------------------------------------------------------"
+    );
+    let dependencyList: RPackage[] = [];
+
+    const dependencyLines = dependencies.split("\n");
+    dependencyLines.forEach((dep) => {  
+      console.debug(dep);
+      if (dep.trim()) {
+        //ignore comments
+        if (dep.startsWith("R packages included in your environment:")) {
+          console.debug("Found comment, skipping");
+        } else {
+          let dependencyParts: string[] = dep.trim().split(" (");
+          let realName: string = dependencyParts[0].substring(2, dependencyParts[0].length);
+          const name: string = realName;
+          const version: string = dependencyParts[1].replace(")", "");
+          if (name && version) {
+            const dependencyObject: RPackage = new RPackage(
+              name,
+              version
+            );
+            dependencyList.push(dependencyObject);
+          } else {
+            console.warn(
+              "Skipping dependency: " +
+                dep +
+                " due to missing data (name, version)"
+            );
+          }
+        }
+      }
+    });
+
+    return dependencyList;
+  }
+}
