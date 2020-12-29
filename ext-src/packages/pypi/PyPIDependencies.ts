6b2e38833f3849e33083924bb8f368b4ee642481
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-12-29 12:47:24 -0900

    Couple more changes

--- ext-src/packages/pypi/PyPIDependencies.ts
+++ ext-src/packages/pypi/PyPIDependencies.ts
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import * as _ from "lodash";
-
 import { PyPIPackage } from "./PyPIPackage";
 import { PackageDependencies } from "../PackageDependencies";
 import { ComponentEntry } from "../../models/ComponentEntry";
