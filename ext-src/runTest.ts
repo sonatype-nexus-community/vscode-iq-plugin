053775925f12e3d7840bcfe63d664dc3d5d9a02e
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-01-13 10:31:30 -0900

    Ok some progress

--- ext-src/runTest.ts
+++ ext-src/runTest.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as path from 'path';
 
 import { runTests } from 'vscode-test';
@@ -15,10 +30,13 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, '../../');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({ 
+			version: '1.46.1',
+			extensionDevelopmentPath, extensionTestsPath 
+			});
 	} catch (err) {
+		console.error(err)
 		console.error('Failed to run tests');
-		process.exit(1);
 	}
 }
 
