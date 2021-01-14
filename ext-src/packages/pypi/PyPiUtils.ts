2d1469569f242ecc55e352431a471d7d15b1f263
Artemie Jurgenson <ajurgenson@sonatype.com>
2021-01-14 12:48:21 -0500

    comment to explain the change

--- ext-src/packages/pypi/PyPiUtils.ts
+++ ext-src/packages/pypi/PyPiUtils.ts
@@ -44,6 +44,7 @@ export class PyPiUtils {
         if (dep.startsWith("#")) {
           console.debug("Found comment, skipping");
         } else {
+          // remove any conditionals after semicolon, split result to get package and version
           const dependencyParts: string[] = dep.split(";")[0].trim().split("==");
           if (!dependencyParts || dependencyParts.length != 2) {
             // Short circuit, we couldn't split, move on to next one
