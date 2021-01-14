2a438638ae876f95e3de275ab29c505457d4cd70
Maury Cupitt <mcupitt@sonatype.com>
2021-01-14 12:50:17 -0500

    fix: Handle semicolons in auto-generated requirements.txt files (#175)
    * split the dep on semicolon in requirements.txt
    
    * comment to explain the change
    
    Co-authored-by: Artemie Jurgenson <ajurgenson@sonatype.com>

--- ext-src/packages/pypi/PyPiUtils.ts
+++ ext-src/packages/pypi/PyPiUtils.ts
@@ -44,7 +44,8 @@ export class PyPiUtils {
         if (dep.startsWith("#")) {
           console.debug("Found comment, skipping");
         } else {
-          const dependencyParts: string[] = dep.trim().split("==");
+          // remove any conditionals after semicolon, split result to get package and version
+          const dependencyParts: string[] = dep.split(";")[0].trim().split("==");
           if (!dependencyParts || dependencyParts.length != 2) {
             // Short circuit, we couldn't split, move on to next one
             return;
