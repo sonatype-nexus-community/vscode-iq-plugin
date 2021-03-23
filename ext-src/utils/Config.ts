3ce96a03bf284f9c725ed64966332baae81e0c18
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-03-23 11:56:10 -0800

    .yml too because why not

--- ext-src/utils/Config.ts
+++ ext-src/utils/Config.ts
@@ -17,7 +17,7 @@ import { existsSync, readFileSync } from "fs";
 import { load } from "js-yaml";
 import { join } from "path";
 import { PackageDependenciesHelper } from "../packages/PackageDependenciesHelper";
-import { DOT_YAML_EXTENSION, SonatypeConfig, SONATYPE_CONFIG_FILE_NAME } from "../types/SonatypeConfig";
+import { DOT_YAML_EXTENSION, DOT_YML_EXTENSION, SonatypeConfig, SONATYPE_CONFIG_FILE_NAME } from "../types/SonatypeConfig";
 
 const NEXUS_EXPLORER_BASE = "nexusExplorer";
 const NEXUS_IQ_BASE = "nexusIQ";
@@ -32,30 +32,42 @@ const NEXUS_IQ_STRICT_SSL = NEXUS_IQ_BASE.concat(".", "strictSSL");
 
 
 /**
- * LoadSonatypeConfig will look for the existence of `.sonatype-config` or
- * `.sonatype-config.yaml`, attempt to load it into a SonatypeConfig interface
+ * LoadSonatypeConfig will look for the existence of `.sonatype-config`,
+ * `.sonatype-config.yaml`, or `.sonatype-config.yml`, in that order, attempt to load it into a SonatypeConfig interface
  * and return it. If nothing is found, it returns undefined.
  * 
  * @returns SonatypeConfig or undefined if no config found
  */
 const LoadSonatypeConfig = (): SonatypeConfig | undefined => {
-    const sonatypeConfigPath = join(PackageDependenciesHelper.getWorkspaceRoot(), SONATYPE_CONFIG_FILE_NAME);
+  /* 
+  * TODO: this is a bit silly for three files that are more or less the same, we should switch to checking for a glob or something
+  * However, left it as this for now so I could have some time to find the best library to do so, or use node.js libraries itself
+  */ 
+  const sonatypeConfigPath = join(PackageDependenciesHelper.getWorkspaceRoot(), SONATYPE_CONFIG_FILE_NAME);
 
-    const sonatypeConfigWithYamlPath = join(PackageDependenciesHelper.getWorkspaceRoot(), SONATYPE_CONFIG_FILE_NAME, DOT_YAML_EXTENSION);
-    
-    if (existsSync(sonatypeConfigPath)) {
-      const doc = load(readFileSync(sonatypeConfigPath, 'utf8')) as SonatypeConfig;
+  const sonatypeConfigWithYamlPath = join(PackageDependenciesHelper.getWorkspaceRoot(), SONATYPE_CONFIG_FILE_NAME, DOT_YAML_EXTENSION);
 
-      return doc;
-    }
+  const sonatypeConfigWithYmlPath = join(PackageDependenciesHelper.getWorkspaceRoot(), SONATYPE_CONFIG_FILE_NAME, DOT_YML_EXTENSION);
+  
+  if (existsSync(sonatypeConfigPath)) {
+    const doc = load(readFileSync(sonatypeConfigPath, 'utf8')) as SonatypeConfig;
 
-    if (existsSync(sonatypeConfigWithYamlPath)) {
-      const doc = load(readFileSync(sonatypeConfigWithYamlPath, 'utf8')) as SonatypeConfig;
+    return doc;
+  }
 
-      return doc;
-    }
+  if (existsSync(sonatypeConfigWithYamlPath)) {
+    const doc = load(readFileSync(sonatypeConfigWithYamlPath, 'utf8')) as SonatypeConfig;
 
-    return undefined;
+    return doc;
+  }
+
+  if (existsSync(sonatypeConfigWithYmlPath)) {
+    const doc = load(readFileSync(sonatypeConfigWithYmlPath, 'utf8')) as SonatypeConfig;
+
+    return doc;
+  }
+
+  return undefined;
 };
 
 export {
