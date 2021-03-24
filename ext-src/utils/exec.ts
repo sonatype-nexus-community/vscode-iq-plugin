7eaa72f622c524df32ed6de5b6b84775a967db51
James Morgan <51096720+jpm63@users.noreply.github.com>
2021-03-24 13:59:01 -0400

    fix: Merge Process Environment on Exec Calls (#192)
    💥

--- ext-src/utils/exec.ts
+++ ext-src/utils/exec.ts
@@ -16,6 +16,8 @@
 import * as cp from 'child_process';
 
 export function exec(command: string, options: cp.ExecOptions): Promise<{ stdout: string; stderr: string }> {
+	options.env = { ...process.env, ...options.env }
+
 	return new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
 		cp.exec(command, options, (error, stdout, stderr) => {
 			if (error) {
